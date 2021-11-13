8af1ff8cfff98f0babaa91a2887b8bdb73e4c58e
leoDlopez <lopez_lopez_daniel@hotmail.com>
2021-11-12 21:40:48 -0600

    Git ignore

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "35786c4e93c1a6ca"; }
+/******/ 		__webpack_require__.h = function() { return "4f6b14d41006d319"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
